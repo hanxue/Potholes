417950c40c44b79c96a0476aa275af69c1099013
sweemeng <sweemeng@sweemeng-centurydevbox.(none)>
2011-07-30 20:17:31 +0800

    views for leaderboard

--- game/engine/views.py
+++ game/engine/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from game.engine.models import Character
+
+def top_experience(request):
+    ch = Character.objects.all().order_by('experience')[0:11]
+    return render_to_response('leaderboard.html',{'character':ch},
+		context_instance = RequestContext(request))
+
+
